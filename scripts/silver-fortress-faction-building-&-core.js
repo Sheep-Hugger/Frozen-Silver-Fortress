6f582fec2ce34f4e1637c42821b0dde087431420
Sheep-Hugger <76137151+Sheep-Hugger@users.noreply.github.com>
2022-02-14 16:57:52 -0600

    Add files via upload

--- scripts/silver-fortress-faction-building-&-core.js
+++ scripts/silver-fortress-faction-building-&-core.js
@@ -1,1 +1,34 @@
-//buildings and core
+//Global faction value
+GlobalFactionSF: 0,
+
+//Faction status
+
+//T1
+//const core_fort = extend(CoreBlock, "core-fort", {
+  //name: "Core: Fort",
+  //description: "???.",
+  //unitType: "alpha",
+  //unitCapModifier: 16,
+  //health: 2200,
+  //itemCapacity: 2000,
+  //size: 3,
+  //core_fort.requirements(Category.effect, ItemStack.with(Items.deep-silver, 2000,));
+  //research: "core-foundation"
+  //});
+
+//core_fort.CoreBlock.CoreBuild = () => extend(CoreBlock, {
+//placed(){
+//this.super$placed();
+//GlobalFactionSF += 1
+//}
+//removed(){
+  //this.super$removed();
+//GlobalFactionSF -= 1
+//}
+//}
+
+//T2
+
+//T3
+
+//GlobalFactionSF counter
